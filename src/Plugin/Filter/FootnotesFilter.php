3c7b9b09c032bfdc383e0290a9a81f4eb7a84624
Florian Latzel <florian.latzel@reinblau.de>
2015-10-16 10:25:36 +0200

    Added empty class Drupal\footnotes\Plugin\FootnotesFilter.

---
+++ src/Plugin/Filter/FootnotesFilter.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\footnotes\Plugin\Filter\FootnotesFilter.
+ */
+
+namespace Drupal\footnotes\Plugin\FootnotesFilter;
+
+
+use Drupal\filter\Plugin\FilterBase;
+
+class FootnotesFilter extends FilterBase{
+
+}
